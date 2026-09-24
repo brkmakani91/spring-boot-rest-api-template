bb910332a0220029e4c7340b9eb160d04efe74aa
brkmakani91 <makani@belbenisolution.com>
2026-09-24 14:02:42 +0200

    feat: add Task entity

---
+++ src/main/java/com/belbenisolution/springbootrestapitemplate/entity/Task.java
@@ -0,0 +1,63 @@
+package com.belbenisolution.springbootrestapitemplate.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String description;
+
+    private boolean done = false;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    void onCreateDate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Task(){
+        // TO DO
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public boolean isDone() {
+        return done;
+    }
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+    public LocalDateTime getDate() {
+        return createdAt;
+    }
+    public void setDate(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
