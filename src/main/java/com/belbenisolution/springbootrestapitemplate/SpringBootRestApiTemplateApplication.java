ccfd07208ccd0ede07913f2f8582bc10192e9580
benidev <belbenimakani@gmail.com>
2026-09-24 12:27:20 +0200

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/belbenisolution/springbootrestapitemplate/SpringBootRestApiTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.belbenisolution.springbootrestapitemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/belbenisolution/springbootrestapitemplate/SpringBootRestApiTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belbenisolution.springbootrestapitemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApiTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
